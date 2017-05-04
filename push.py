829cfd555803c6734eb9cd359e57918eb2c134e2
amrutharamesh <docdagny@gmail.com>
2017-05-04 16:40:39 -0500

    changes

---
+++ push.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import socket
+from urllib2 import *
+import urllib
+import json
+import sys
+
+def send_push():
+    MY_API_KEY="AIzaSyAr6UZ6R9fT0RwDera7adrtSpzgRyYq4CM"
+
+    messageTitle = 'TripleS'
+    messageBody = 'hey bro!'
+
+    data={
+        "to" : "/topics/my_little_topic",
+        "notification" : {
+            "body" : messageBody,
+            "title" : messageTitle,
+            "icon" : "ic_cloud_white_48dp"
+        }
+    }
+
+    dataAsJSON = json.dumps(data)
+
+    request = Request(
+        "https://gcm-http.googleapis.com/gcm/send",
+        dataAsJSON,
+        { "Authorization" : "key="+MY_API_KEY,
+          "Content-type" : "application/json"
+        }
+    )
+
+    print urlopen(request).read()
+
+send_push()
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var http = require("http");
 var url = require('url');
 var fs = require('fs');
 var io = require('socket.io');
+var net = require('net');
 
+//HTTP SERVER
 var server = http.createServer(function(request, response){
     var path = url.parse(request.url).pathname;
 
@@ -123,15 +125,53 @@ var server = http.createServer(function(request, response){
 server.listen(2000);
 
 var listener = io.listen(server);
+ 
+  listener.sockets.on('connection', function(socket){
+     socket.emit('message', {'message': 'hello world'});
+   //send data to client
+   setInterval(function(){
+     socket.emit('date', {'date': new Date()});
+   }, 1000);
+  
+     //recieve client data
+       socket.on('client_data', function(data){
+         process.stdout.write(data.letter);
+       });
+   //recieve client data
+   socket.on('client_data', function(data){
+     process.stdout.write(data.letter);
+   });
+}); 
 
-listener.sockets.on('connection', function(socket){
-  //send data to client
-  setInterval(function(){
-    socket.emit('date', {'date': new Date()});
-  }, 1000);
 
-  //recieve client data
-  socket.on('client_data', function(data){
-    process.stdout.write(data.letter);
-  });
-});
+//TELNET SERVER
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 6971;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        var gasValue = data.toString("utf8", 0, data.length - 1);
+        console.log(gasValue);
+        var spawn = require("child_process").spawn;
+        var process = spawn('python',["push.py"]);
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
