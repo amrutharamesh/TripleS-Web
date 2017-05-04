29d1a4b0c0a0c086b33d19207824a9b9b443ee90
amrutharamesh <docdagny@gmail.com>
2017-05-04 12:26:22 -0500

    adding changes

--- server.js
+++ server.js
@@ -5,42 +5,119 @@ var io = require('socket.io');
 
 var server = http.createServer(function(request, response){
     var path = url.parse(request.url).pathname;
+
+    console.log(request.url)
     
-    switch(path){
-        case '/':
-             fs.readFile(__dirname + '/home.html', function(error, data){
-                if (error){
-                    response.writeHead(404);
-                    response.write("opps this doesn't exist - 404");
-                    response.end();
-                }
-                else{
-                    response.writeHead(200, {"Content-Type": "text/html"});
-                    response.write(data, "utf8");
-                    response.end();
-                }
-            });
-            break;
-        case '/socket':
-            fs.readFile(__dirname + path + '.html', function(error, data){
-                if (error){
-                    response.writeHead(404);
-                    response.write("opps this doesn't exist - 404");
-                    response.end();
-                }
-                else{
-                    response.writeHead(200, {"Content-Type": "text/html"});
-                    response.write(data, "utf8");
-                    response.end();
-                }
-            });
-            break;
-        default:
-            response.writeHead(404);
-            response.write("opps this doesn't exist - 404");
-            response.end();
-            break;
+    if(request.url.indexOf('.html') != -1){
+        switch(path){
+            case '/home.html':
+                 fs.readFile(__dirname + path, function(error, data){
+                    if (error){
+                        response.writeHead(404);
+                        response.write("opps this doesn't exist - 404");
+                        response.end();
+                    }
+                    else{
+                        response.writeHead(200, {"Content-Type": "text/html"});
+                        response.write(data, "utf8");
+                        response.end();
+                    }
+                });
+                break;
+            case '/socket.html':
+                fs.readFile(__dirname + path, function(error, data){
+                    if (error){
+                        response.writeHead(404);
+                        response.write("opps this doesn't exist - 404");
+                        response.end();
+                    }
+                    else{
+                        response.writeHead(200, {"Content-Type": "text/html"});
+                        response.write(data, "utf8");
+                        response.end();
+                    }
+                });
+                break;
+            case '/inventory.html':
+                fs.readFile(__dirname + path, function(error, data){
+                    if (error){
+                        response.writeHead(404);
+                        response.write("opps this doesn't exist - 404");
+                        response.end();
+                    }
+                    else{
+                        response.writeHead(200, {"Content-Type": "text/html"});
+                        response.write(data, "utf8");
+                        response.end();
+                    }
+                });
+                break;
+            default:
+                response.writeHead(404);
+                response.write("opps this doesn't exist - 404");
+                response.end();
+                break;
+        }
     }
+    if(request.url.indexOf('.css') != -1){
+        switch(path){
+            case '/app.css' : 
+                fs.readFile(__dirname + path, function (err, data) {
+                    if (err) console.log(err);
+                    response.writeHead(200, {'Content-Type': 'text/css'});
+                    response.write(data);
+                    response.end();
+                });
+                break;
+            case '/node_modules/bootstrap/dist/css/bootstrap.min.css' : 
+                fs.readFile(__dirname + path, function (err, data) {
+                    if (err) console.log(err);
+                    response.writeHead(200, {'Content-Type': 'text/css'});
+                    response.write(data);
+                    response.end();
+                });
+                break;
+            case '/node_modules/font-awesome/css/font-awesome.min.css' : 
+                fs.readFile(__dirname + path, function (err, data) {
+                    if (err) console.log(err);
+                    response.writeHead(200, {'Content-Type': 'text/css'});
+                    response.write(data);
+                    response.end();
+                });
+                break;
+            default:
+                response.writeHead(404);
+                response.write("opps this doesn't exist - 404");
+                response.end();
+                break;
+        } 
+    }
+    if(request.url.indexOf('.woff') != -1){
+        fs.readFile(__dirname + '/node_modules/font-awesome/fonts/fontawesome-webfont.woff2', function (err, data) {
+            if (err) console.log(err);
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.write(data);
+            response.end();
+        });
+    }
+    if(request.url.indexOf('.js') != -1){
+        switch(path){
+            case '/node_modules/bootstrap/dist/js/bootstrap.min.js' :
+                fs.readFile(__dirname + path, function (err, data) {
+                    if (err) console.log(err);
+                    response.writeHead(200, {'Content-Type': 'text/javascript'});
+                    response.write(data);
+                    response.end();
+                });
+            break;
+            default:
+                response.writeHead(404);
+                response.write("opps this doesn't exist - 404");
+                response.end();
+                break;
+        }
+    }
+    
 });
 
 server.listen(2000);
